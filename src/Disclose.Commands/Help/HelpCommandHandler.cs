1b125fdbfc7143058b7b9f5812cc52dc9f94ab5b
Seaal <Seaal@users.noreply.github.com>
2016-11-17 14:25:05 +0000

    Adding help command

---
+++ src/Disclose.Commands/Help/HelpCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+using Disclose.DiscordClient;
+
+namespace Disclose.Commands.Help
+{
+    public class HelpCommandHandler : CommandHandler
+    {
+        public override string CommandName => "help";
+        public override string Description => "Gives information about the commands available to use for this bot";
+
+        public override async Task Handle(IMessage message, string arguments)
+        {
+            string helpMessage;
+
+            if (String.IsNullOrWhiteSpace(arguments))
+            {
+                helpMessage = GetHelpForAllCommands(message.User);
+            }
+            else
+            {
+                ICommandHandler commandHandler = Disclose.CommandHandlers.FirstOrDefault(ch => ch.CommandName == arguments.ToLowerInvariant());
+
+                helpMessage = GetHelpForCommand(commandHandler, message.User) ?? "Command does not exist";
+            }
+
+            await Discord.SendMessageToUser(message.User, helpMessage);
+        }
+
+        private string GetHelpForAllCommands(IUser user)
+        {
+            string helpMessage = "Here are the commands available for you:\n\n";
+
+            foreach (ICommandHandler commandHandler in Disclose.CommandHandlers)
+            {
+                string commandHelpMessage = GetHelpForCommand(commandHandler, user);
+
+                if (commandHelpMessage != null)
+                {
+                    helpMessage += $"{commandHelpMessage}\n";
+                }
+            }
+
+            return helpMessage;
+        }
+
+        private string GetHelpForCommand(ICommandHandler commandHandler, IUser user)
+        {
+            if (commandHandler != null && (commandHandler.UserFilter == null || commandHandler.UserFilter(user)))
+            {
+                return $"**{commandHandler.CommandName}** - {commandHandler.Description}";
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/Disclose.Commands/Help/HelpInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disclose.Commands.Help
+{
+    public class HelpInstaller : IInstaller
+    {
+        public void Install(DiscloseClient discoseClient)
+        {
+            discoseClient.Register(new HelpCommandHandler());
+        }
+    }
+}
