263be7e94d157ba7d56b5909d6e99309eb9cf868
Seaal <Seaal@users.noreply.github.com>
2017-01-04 20:30:27 +0000

    Added Installers for Gamble and WelcomeMessage

---
+++ src/Disclose.Commands/Gamble/GambleInstaller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disclose.Commands.Gamble
+{
+    public class GambleInstaller : IInstaller
+    {
+        private readonly GambleOptions _options;
+
+        public GambleInstaller()
+        {
+            _options = new GambleOptions();
+        }
+
+        public GambleInstaller(Action<GambleOptions> setOptions) : this()
+        {
+            setOptions(_options);
+        }
+
+        public void Install(DiscloseClient discoseClient)
+        {
+            discoseClient.Register(new GambleCommandHandler(_options));
+        }
+    }
+}
---
+++ src/Disclose.Commands/WelcomeMessage/WelcomeMessageInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disclose.Commands.WelcomeMessage
+{
+    public class WelcomeMessageInstaller : IInstaller
+    {
+        private readonly IMessageStrategy _messageStrategy;
+
+
+        public WelcomeMessageInstaller(IMessageStrategy messageStrategy)
+        {
+            _messageStrategy = messageStrategy;
+        }
+
+        public void Install(DiscloseClient discoseClient)
+        {
+            discoseClient.Register(new WelcomeMessageHandler(_messageStrategy));
+        }
+    }
+}
